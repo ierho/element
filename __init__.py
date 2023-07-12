9dcbad0196bec4cf3f80358c4b8c33eff16f534d
ierhon <ierhon@proton.me>
2023-07-12 22:44:22 +0600

    Improve event system

--- __init__.py
+++ __init__.py
@@ -1,14 +1,19 @@
 from matrix_client.client import MatrixClient
 
 
-events = ['on_message', 'on_ready']
+events = ['on_message', 'on_ready', 'on_cipher']
 
 
 class Bot:
-    def __init__(self, username: str, password: str):
+    def __init__(self, username: str, password: str, prefix=".", log_function=lambda text: print("WARNING:", text)):
         self.events = {}
+        self.log = log_function
+        self.prefix = prefix
+        for i in events:
+            self.events[i] = lambda ctx: None
         self.client = MatrixClient("https://matrix-client.matrix.org")
-        self.client.login_with_password(username=username, password=password)
+        self.client.login(username=username, password=password, sync=True)
+        self.running = True
 
     def event(self, func):
         name = func.__name__
@@ -19,14 +24,17 @@ class Bot:
             pass  # raise an error
 
     def listener(self, ctx):
-        for event in self.events.values():
-            event(ctx)
+        if ctx['type'] == "m.room.message":
+            self.events['on_message'](ctx)
+        elif ctx['type'] == "m.room.encrypted":
+            self.events['on_cipher'](ctx)
+        else:
+            self.log("Unknown event")
 
     def run(self, loop=True):
         self.client.add_listener(self.listener)
         self.client.start_listener_thread()
-        if 'on_ready' in self.events.keys():
-            self.events['on_ready']()
+        self.events['on_ready']()
         if loop:
             while self.running:
                 pass
