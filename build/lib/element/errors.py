f098b79df4048e5605f0a6dc0a6838dea2728a88
ierhon <ierhon@proton.me>
2023-07-13 20:35:06 +0600

    Remove unnecessary files

--- build/lib/element/errors.py
+++
@@ -1,8 +0,0 @@
-class UnknownEvent(Exception):
-    """Raised when an invalid event is used"""
-    pass
-
-
-class EmptyMessage(Exception):
-    """Raised when an empty message is trying to be sent"""
-    pass
